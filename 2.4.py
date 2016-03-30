200b3abcebfe7a2451ccf70001fe53949e874d0f
Max li <wyx5205217@gmail.com>
2016-03-30 22:01:51 +0800

    a1 assignment

---
+++ 2.4.py
@@ -0,0 +1,14 @@
+#1
+def printChars(string):
+    print("\n".join(string))
+
+#2
+def emailDomain(string):
+    a=string.find('@')
+    return string[a+1:]
+
+
+
+def printSorted(string1,string2):
+    if string1<string2
+        print(strint1
---
+++ x.py
@@ -0,0 +1,10 @@
+def diff(x,y):
+    return float(x)-float(y)
+diff (0.3,0.1+0.2)
+
+def inEqual(a,b):
+    return abs(a-b)<10**-10
+
+
+import sys
+sys.float_info.max
